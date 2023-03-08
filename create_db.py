f7072d1c5572bd019096c8a1478910c072236216
Ali Mughrabi <alicool040@gmail.com>
2023-03-08 05:53:57 +0000

    - comment

--- create_db.py
+++ create_db.py
@@ -4,3 +4,10 @@ from app.models import User
 # create tables for ORM (user table only)
 with app.app_context():
     db.create_all()
+
+# this equal:
+# CREATE TABLE user (
+#     id INT PRIMARY KEY AUTO_INCREMENT,
+#     username VARCHAR(255) NOT NULL UNIQUE,
+#     password VARCHAR(255) NOT NULL,
+# );
